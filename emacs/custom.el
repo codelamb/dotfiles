7369a1f8a8088e16f4c4f626f6addb7d6cfd22a6
codelamb <12115250+codelamb@users.noreply.github.com>
2019-09-28 14:11:12 +0300

    Emacs custom.el
    Probably won't need it, just in case.

---
+++ emacs/custom.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#111111" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(blink-cursor-mode nil)
+ '(bongo-enabled-backends (quote (mplayer)))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-safe-themes
+   (quote
+    ("ea21ac2e2a4d55aa8ae26409a34aa1d4ef9e8dae72bd921e77e6e651b4927312" "8c83a20c335adf40eee2f8d0174026b62c703ee9f4c6543cca39a2ae87c959aa" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
+ '(package-selected-packages (quote (yasnippet)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(ring-bell-function (quote ignore))
+ '(safe-local-variable-values (quote ((org-drill-maximum-items-per-session . 100))))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(blink-cursor-mode 0)
+(setq org-drill-save-buffers-after-drill-sessions-p nil)
+
+(global-set-key (kbd "C-x g") 'magit-status)
