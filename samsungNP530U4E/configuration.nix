134673f60173eaf0464faa6feb0c7cafc58971bc
codelamb <12115250+codelamb@users.noreply.github.com>
2021-07-28 13:35:51 +0300

    Forced update after upgrade to 21.05 fail...
    ... and bricking the system. Possible cause - missing tools (mtools) to deal with fat32 filesystems.

--- samsungNP530U4E/configuration.nix
+++ samsungNP530U4E/configuration.nix
@@ -1,116 +1,146 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ).
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
- imports =
-   [ # Include the results of the hardware scan.
-     ./hardware-configuration.nix
-   ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
- # Use the systemd-boot EFI boot loader.
- boot.loader.systemd-boot.enable = true;
- boot.loader.efi.canTouchEfiVariables = true;
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
- # networking.hostName = "nixos"; # Define your hostname.
- #  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
- networking.networkmanager.enable = true; 
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+   networking.networkmanager.enable = true; 
 
- # Configure network proxy if necessary
- # networking.proxy.default = "http://user:password@proxy:port/";
- # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
- # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "lt_LT.UTF-8";
-  };
-
- # Set your time zone.
+  # Set your time zone.
   time.timeZone = "Europe/Vilnius";
 
- # List packages installed in system profile. To search, run:
- # $ nix search wget
-  environment.systemPackages = with pkgs; [
- #   wget vim
-    libreoffice-fresh
-    firefox-bin
-    kdeApplications.okular
-    ark
-    kdeApplications.gwenview
-    xineLib
-    xineUI
-  ];
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
 
- # Some programs need SUID wrappers, can be configured further or are
- # started in user sessions.
- # programs.mtr.enable = true;
- # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
- # List services that you want to enable:
+  # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+      font = "Lat2-Terminus16";
+      keyMap = "us";
+    };
 
- # Enable the OpenSSH daemon.
- # services.openssh.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
- # Open ports in the firewall.
- networking.firewall.allowedTCPPorts = [ 80 443 ];
- # networking.firewall.allowedUDPPorts = [ ... ];
- # Or disable the firewall altogether.
- # networking.firewall.enable = false;
 
- # Enable CUPS to print documents.
- services.printing.enable = true;
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
- # Enable sound.
- sound.enable = true;
- hardware.pulseaudio.enable = true;
+  # Autologin
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "VidaMarija";
 
- # Enable the X11 windowing system.
- services.xserver.enable = true;
- services.xserver.layout = "us";
- # services.xserver.xkbOptions = "eurosign:e";
+  # Configure keymap in X11
+    services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
- # Enable touchpad support.
- services.xserver.libinput.enable = true;
+  # Enable CUPS to print documents.
+    services.printing.enable = true;
 
- # Enable the KDE Desktop Environment.
-   services.xserver.displayManager.sddm.enable = true;
-   services.xserver.displayManager.sddm.autoLogin.enable = true;
-   services.xserver.displayManager.sddm.autoLogin.user = "VidaMarija";
-   services.xserver.desktopManager.plasma5.enable = true;
+  # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
- # Define a user account. Don't forget to set a password with .
-  users.users.VidaMarija = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable  for the user.
-  };
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
- # This value determines the NixOS release with which your system is to be
- # compatible, in order to avoid breaking some software such as database
- # servers. You should change this only after NixOS release notes say you
- # should.
- system.stateVersion = "19.03"; # Did you read the comment?
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.VidaMarija = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    };
 
- # Automatic upgrades
- system.autoUpgrade.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      emacs
+      wget
+      libreoffice
+      firefox
+      chromium
+      okular
+      ark
+      gwenview
+      xineLib
+      xineUI
+      vlc
+      zoom-us
+      zip
+      unzip
+      kcalc #calculator
+      ntfs3g #to read windows formated usb flash drives
+      xclip #Tool to access the X clipboard from a console application
+      lightspark #Open source Flash Player implementation
+      mtools # to read fat32 filesystems
+    ];
 
- # Automatic garbage cleaner
- nix.gc.automatic = true;
- nix.gc.dates = "03:15";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-security.wrappers = {
- firejail = {
-   source = "${pkgs.firejail.out}/bin/firejail";
- };
-};
+  # List services that you want to enable:
 
-# Silent boot
-  boot.consoleLogLevel = 0;
-  boot.loader.timeout = pkgs.lib.mkForce 0;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
+  # Open ports in the firewall.
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Evil
+    nixpkgs.config.allowUnfree = true;
+
+  # Automatic upgrades
+    system.autoUpgrade.enable = true;
+
+  # Automatic garbage cleaner
+    nix.gc.automatic = true;
+    nix.gc.dates = "03:15";
+
+  # Silent boot
+    boot.consoleLogLevel = 0;
+    boot.kernelParams = [ "quiet" "udev.log_priority=3" ];
+    boot.initrd.verbose = false;
+
+  # Splash screen
+    boot.plymouth.enable = true;
+
+  # Security
+  boot.loader.systemd-boot.editor = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
 
 }
