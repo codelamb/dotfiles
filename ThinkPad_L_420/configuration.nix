b1ae3a3d260b9e471c418ed8fb333ac54d9bf685
codelamb <12115250+codelamb@users.noreply.github.com>
2021-12-12 18:14:49 +0200

    Backup before upgrade to 21.11

--- ThinkPad_L_420/configuration.nix
+++ ThinkPad_L_420/configuration.nix
@@ -4,13 +4,15 @@
 
 { config, pkgs, ... }:
 
+let
+  unstable = import <unstable> {};
+in 
 {
  imports =
    [ # Include the results of the hardware scan.
      ./hardware-configuration.nix
    ];
 
-  
   # Use the GRUB 2 boot loader.
     boot.loader.grub.enable = true;
     boot.loader.grub.version = 2;
@@ -19,6 +21,7 @@
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
     boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
     networking.networkmanager.enable = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
@@ -40,13 +43,27 @@
     time.timeZone = "Europe/Vilnius";
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
-    environment.systemPackages = with pkgs; [
+    # $ nix search wget
+
+  # Mozzila
+  nixpkgs.overlays =
+    let
+      # Change this to a rev sha to pin
+      moz-rev = "master";
+      moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
+      nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
+    in [
+      nightlyOverlay
+    ];
+  
+
+environment.systemPackages = with pkgs; [
       wget
-        # (import /etc/nixo/emacs.nix { inherit pkgs; })
-      emacs	
+      # (import /home/darius/.emacs.d/emacs.nix { inherit pkgs; })
+      emacs
       libreoffice
-      firefox
+      # firefox
+      latest.firefox-nightly-bin
       ark
       xineLib
       xineUI
@@ -56,11 +73,11 @@
       hugo
       ledger
       lynx
-      w3m
+      # w3m # removed due multiple vulnerabilities
       chromium
       discord
       gitFull
-      firejail
+      # firejail
       xclip #Tool to access the X clipboard from a console application
       gimp
       scribus
@@ -70,8 +87,10 @@
       ntfs3g #to read windows formated usb flash drives
       android-studio
       multimc
+      minecraft
       lightspark #Open source Flash Player implementation
       gitter
+      gzip
       zip
       unzip
       pandoc
@@ -81,6 +100,30 @@
       okular
       kbdd
       kcalc
+      ispell
+      gnumake 
+      aspell
+      aspellDicts.lt
+      coreutils # for ispell-lt
+      findutils # for ispell-lt
+      python3 # ispell-lt make wants it more than python 2
+      curl #mainly for emacs elfeed
+      vulnix
+      webcamoid
+      unstable.android-tools
+      fishnet # Distributed Stockfish analysis for lichess.org
+      imagemagick # For creating emacs image thumbnails
+      gparted
+      mtools # for reading fat32, for some reason it was not included in 21.05 ISO image
+      openvpn
+      playerctl # for media keys
+      xorg.xev # for maping media keys
+      mullvad-vpn
+      qbittorrent
+      # libstdcxx5 #hope firefox-nightly will wok with it
+      skanlite
+      image_optim
+      libjpeg
     ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -89,22 +132,42 @@
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
+    services.syncthing = {
+      enable = true;
+      user = "darius";
+      dataDir = "/home/darius/.config/syncthing";
+    };
 
+  #  services.openvpn.servers = {
+  #    teismasVPN  = { config = '' config /root/nixos/openvpn/teismas_gw.ovpn ''; };
+  #  };
+    
+    
     security.chromiumSuidSandbox.enable = true;
   
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
-  # networking.firewall.allowedUDPPorts = [ 631 ];
+    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+    networking.firewall.allowedUDPPorts = [ 1194 1325 ]; # for OpenVPN
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # networking.firewall.enable = false;
+
+    networking.firewall.checkReversePath = "loose"; # for Mullvad
+      
+  # Mullvad VPN
+    networking.iproute2.enable = true;
+    services.mullvad-vpn.enable = true;
 
   # Enable CUPS to print documents.
      services.printing.enable = true;
-  #  services.printing.drivers = [ ... ];
-    
+     services.printing.drivers = [ pkgs.cupsBjnp pkgs.samsung-unified-linux-driver ];
+
+  # Scanner
+    hardware.sane.enable = true;
+     
+     
   # Enable sound.
     sound.enable = true;
     hardware.pulseaudio.enable = true;
@@ -142,7 +205,11 @@
 
   # EVIL
     nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config.allowBroken = true;
+    # nixpkgs.config.allowBroken = true;
+    nixpkgs.config.permittedInsecurePackages = [
+        "python2.7-Pillow-6.2.2"
+    ];
+
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
