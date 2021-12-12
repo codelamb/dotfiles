f57438d776fe9847e795449a7f9992484ecaaa8f
codelamb <12115250+codelamb@users.noreply.github.com>
2021-12-12 18:23:31 +0200

    Backup before moving to Nixos 21.11

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,56 +3,113 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#111111" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(bidi-paragraph-direction 'left-to-right)
  '(blink-cursor-mode nil)
- '(bongo-enabled-backends (quote (mplayer)))
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(custom-safe-themes
-   (quote
-    ("ea21ac2e2a4d55aa8ae26409a34aa1d4ef9e8dae72bd921e77e6e651b4927312" "8c83a20c335adf40eee2f8d0174026b62c703ee9f4c6543cca39a2ae87c959aa" default)))
- '(fci-rule-color "#383838")
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(org-export-backends (quote (ascii html icalendar latex md odt)))
- '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
- '(package-selected-packages (quote (yasnippet)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(ring-bell-function (quote ignore))
- '(safe-local-variable-values (quote ((org-drill-maximum-items-per-session . 100))))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+ '(browse-url-browser-function 'browse-url-firefox)
+ '(elfeed-search-title-max-width 100)
+ '(elfeed-search-title-min-width 100)
+ '(newsticker-url-list '(("Kontanta-42" "www.konstanta.lt/rss" nil nil nil)))
+ '(org-archive-location "~/org/archive.org::")
+ '(w3m-search-default-engine "duckduckgo"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(blink-cursor-mode 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; desktop save session ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(desktop-save-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-drill settings                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq org-drill-save-buffers-after-drill-sessions-p nil)
 
+;;  To enable random "noise" for item intervals in org-drill
+(setq org-drill-add-random-noise-to-intervals-p t)
+
+
+
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(setq org-todo-keywords
+     '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+
+(display-time-mode 1)
+
+(toggle-scroll-bar -1)
+
+(tool-bar-mode -1)
+
+;; (setq org-clock-persist 'history)
+;; (eval-after-load "org"
+;;  '(progn 
+;;   (org-clock-persistence-insinuate)))
+
+;; lietuviškos kabutės
+(add-hook 'org-mode-hook 'electric-quote-mode)
+(setq electric-quote-replace-double t 
+      electric-quote-chars '(8216 8217 8222 8220))
+
+;; data is stored in ~/.elfeed
+(setq elfeed-feeds
+      '(
+        ;; openhardware movement
+        ("https://linuxsmartphones.com/feed/" linux smartphones)
+	("https://tuxphones.com/rss/" linux smartphones)
+
+        ;; science
+        ("http://www.konstanta.lt/feed/atom" astrofizika)
+
+	;; politics
+	("https://www.svoboda.org/api/z_jqpperyop_" svoboda)
+	("http://feeds2.feedburner.com/racas" racas)
+	("http://racas.lt/feed/rss/" racas)
+	
+	;; security
+	("https://www.schneier.com/feed/atom/" security)
+	("https://krebsonsecurity.com/feed/" security)
+
+	;; psichologija
+	("https://www.advancedassessments.co.uk/Blog/files/feed.xml" psichologija teisė)
+	("https://www.criminalthinking.net/feed/" psichologija teisė)
+	("https://www.bulletpsych.com/blog-feed.xml" psichologija psichiatrija)
+	("http://popsych.org/feed/" psichologija)
+	("https://danariely.com/feed/" psichologija)
+	("http://humanfactorsblog.org/feed/" psichologija)
+	("https://mindhacks.com/feed/" psichologija)
+
+	;; IT
+	("https://sachachua.com/blog/feed/" emacs)
+	("https://functor.tokyo/blog/feed/" nixos)
+	("https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml" emacs)
+	
+))
+
+;; (setq-default elfeed-search-filter "@2-days-ago +unread")
+(setq-default elfeed-search-title-max-width 100)
+(setq-default elfeed-search-title-min-width 100)
+
+
+;; emms controls keybindings
+(global-set-key (kbd "C-c e <up>") 'emms-start)
+(global-set-key (kbd "C-c e <down>") 'emms-stop)
+(global-set-key (kbd "C-c e <left>") 'emms-previous)
+(global-set-key (kbd "C-c e <right>") 'emms-next)
+(global-set-key (kbd "<XF86AudioNext>") 'emms-seek-forward)
+(global-set-key (kbd "<XF86AudioPrev>") 'emms-seek-backward)
+(global-set-key (kbd "<XF86AudioPlay>") 'emms-pause) 
+
+;; emacs window to always show your system-name and the full path of the buffer you're currently editing
+
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+            '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
+
+;; copy command automatically select the target dir in a split pane
+(setq dired-dwim-target t)
